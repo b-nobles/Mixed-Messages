a55cdb9de1c304c88d6a18e3bd72db19ed075453
Brandon Nobles <bjnobles10@gmail.com>
2023-03-17 11:38:09 -0400

    Add reset button functionality

--- app.js
+++ app.js
@@ -5,25 +5,38 @@ import {quotes, fortunes, activities} from '/phrases.js'
 const quoteText = document.getElementById('quote');
 const fortuneText = document.getElementById('fortune');
 const activityText = document.getElementById('activity')
+const resetBtn = document.getElementById('reset')
 
 //Create the randomizing function that will be reused
 const getRand = (arr) => Math.floor(Math.random() * arr.length)
 
 //Initialize variables that will that are temporary but can be reused
-let quoteVal = getRand(quotes);
-let fortuneVal = getRand(fortunes);
-let activityVal = getRand(activities);
+let quoteVal = '';
+let fortuneVal = '';
+let activityVal = '';
 
 //Create function that will change website text appropriately
 function updateVars() {
-//Generate the fortune first
-fortuneText.innerHTML = fortunes[fortuneVal].word
-//Give the fortune an appropriate color
-fortuneText.style.color = fortunes[fortuneVal].color
-//Generate the quote second
-quoteText.innerHTML = quotes[quoteVal]
-//Generate the activity third
-activityText.innerHTML = `Maybe try ${activities[activityVal]}`
+    //Generate the fortune first
+    fortuneText.innerHTML = fortunes[fortuneVal].word
+    //Give the fortune an appropriate color
+    fortuneText.style.color = fortunes[fortuneVal].color
+    //Generate the quote second
+    quoteText.innerHTML = quotes[quoteVal]
+    //Generate the activity third
+    activityText.innerHTML = `Maybe try ${activities[activityVal]}`
 }
 
-updateVars();
+//Create reusable function to assign values and then upate text
+function assignVars() {
+    quoteVal = getRand(quotes);
+    fortuneVal = getRand(fortunes);
+    activityVal = getRand(activities);
+    updateVars();
+}
+
+//Call assignVars to have starting value
+assignVars();
+
+//Update the values to change page text
+resetBtn.addEventListener('click', assignVars)
